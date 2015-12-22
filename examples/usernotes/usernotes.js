58b8aebbe15ba976ed7a949d3ec5fca97dcbebdc
a31859 <delioamaral13@gmail.com>
2015-12-22 11:21:26 +0000

    added Notes and example

--- examples/usernotes/usernotes.js
+++ examples/usernotes/usernotes.js
@@ -1,4 +1,52 @@
 (function(Backbone, Notes) {
-  'use strict';
+    'use strict';
+
+    console.log('NOTES TEST');
+
+    var User = Backbone.Model.extend({
+        defaults: {
+            name: 'unnamed'
+        },
+        urlRoot: '/users'
+    });
+
+    var user = new User({
+        id: 1,
+        name: 'John Doe'
+    });
+
+    var userNotes = new Notes.Collection([], {
+        parentModel: user,
+        //url: '/mynotes' // custom URL
+    });
+
+    //console.log('Before fetch -', userNotes);
+
+    userNotes.fetch();
+
+    //console.log('After fetch all -', userNotes);
+
+    console.log('Fetch URL -', userNotes.url());
+
+    // add note to user
+    userNotes.add({
+        text: 'This is a new note'
+    });
+    userNotes.at(0).save();
+
+
+    // edit user note
+    userNotes.set({
+        id: "1",
+        text: "Note edited"
+    });
+    userNotes.at(0).save();
+
+    // delete user note
+    userNotes.at(0).destroy();
+
+    console.log('save URL -', userNotes.at(0).url());
+
+    console.log('Notes -', userNotes);
 
 })(Backbone, Notes);
--- index.js
+++ index.js
@@ -1,18 +1,66 @@
 (function(root, main) {
-  // AMD
-  if (typeof define === 'function' && define.amd) {
-    define(['backbone'],
-      function(Backbone) {
-        return main(Backbone);
-      });
-    // CommonJS
-  } else if (typeof exports !== 'undefined' && typeof require !== 'undefined') {
-    module.exports = main(require('backbone'));
-    // Globals
-  } else {
-    root.Notes = main(root.Backbone);
-  }
+    // AMD
+    if (typeof define === 'function' && define.amd) {
+        define(['backbone'],
+            function(Backbone) {
+                return main(Backbone);
+            });
+        // CommonJS
+    } else if (typeof exports !== 'undefined' && typeof require !== 'undefined') {
+        module.exports = main(require('backbone'));
+        // Globals
+    } else {
+        root.Notes = main(root.Backbone);
+    }
 })(this, function(Backbone) {
-  'use strict';
+    'use strict';
 
+    var Notes = Object.create(null);
+
+    Notes.Model = Backbone.Model.extend({
+        defaults: {
+            text: ""
+        },
+
+        initialize: function() {
+            // if note type is invalid log error to console
+            this.on("invalid", function(model, error) {
+                console.error(error.name, error.message);
+            });
+        },
+
+        /**
+         * Checks if the note text is a string
+         * @return {Object} with the error name and message
+         */
+        validate: function(attrs, options) {
+            if (typeof attrs.text !== 'string') {
+                return {
+                    name: "Invalid parameter:",
+                    message: "The 'text' must be a string."
+                };
+            }
+        }
+    });
+
+    /**
+     * Collection of Notes associated with a parent Model
+     */
+    Notes.Collection = Backbone.Collection.extend({
+        model: Notes.Model,
+        // initialize parentModel and collection URL
+        initialize: function(models, options) {
+            this.parentModel = options.parentModel;
+            this._url = options.url;
+        },
+        /**
+         * Associate collection URL with the parent Model URL
+         * @return {string | undefined}
+         */
+        url: function() {
+            return this.parentModel ? this.parentModel.url() + (this._url || '/notes') : undefined;
+        }
+    });
+
+    return Notes;
 });
