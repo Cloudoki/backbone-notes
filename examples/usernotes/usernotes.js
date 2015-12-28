e8eb9120b0ce2e146e5ba988a5492c3548f43906
a31859 <delioamaral13@gmail.com>
2015-12-28 10:50:53 +0000

    Remove log error to console, add example results

--- examples/usernotes/usernotes.js
+++ examples/usernotes/usernotes.js
@@ -17,36 +17,37 @@
 
     var userNotes = new Notes.Collection([], {
         parentModel: user,
-        //url: '/mynotes' // custom URL
+        // url: '/mynotes' // custom URL
     });
 
-    //console.log('Before fetch -', userNotes);
-
     userNotes.fetch();
-
-    //console.log('After fetch all -', userNotes);
+    // GET /users/1/(notes or custom URL), may show Error 404
 
     console.log('Fetch URL -', userNotes.url());
+    // should show in console -> Fetch URL - /users/1/(notes or custom URL)
 
     // add note to user
     userNotes.add({
         text: 'This is a new note'
     });
     userNotes.at(0).save();
+    // POST to endpoint /users/1/(notes or custom URL), may show Error 501
 
-
-    // edit user note
+   // edit user note
     userNotes.set({
-        id: "1",
-        text: "Note edited"
+        id: '2',
+        text: 'Note edited'
     });
     userNotes.at(0).save();
+    // PUT to endpoint /users/1/(note or custom URL)/2, may show Error 501
+
+    console.log('save URL -', userNotes.at(0).url());
+    // should show in console -> save URL - /users/1/(notes or custom URL)/2
 
     // delete user note
     userNotes.at(0).destroy();
+    // DELETE to endpoint /users/1/(notes or custom URL)/2, may show Error 501
 
-    console.log('save URL -', userNotes.at(0).url());
-
-    console.log('Notes -', userNotes);
+    // console.log('Notes -', userNotes);
 
 })(Backbone, Notes);
